9b4dde2ca0fa2b860519652ca1d3690c7bd5bb6f
Porama Arsama <porama.prm@gmail.com>
2024-05-11 00:49:29 +0700

    Update class_maps.service.ts

--- src/class_maps/class_maps.service.ts
+++ src/class_maps/class_maps.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateClassMapDto } from './dto/create-class_map.dto';
 import { UpdateClassMapDto } from './dto/update-class_map.dto';
+import { class_maps } from './schemas/class_maps.model';
 
 @Injectable()
 export class ClassMapsService {
-  create(createClassMapDto: CreateClassMapDto) {
-    return 'This action adds a new classMap';
+  constructor(
+    @InjectModel(class_maps) private class_mapsModel: typeof class_maps,
+  ) {}
+
+  async create(createClassMapDto: CreateClassMapDto): Promise<class_maps> {
+    try {
+      const result = await this.class_mapsModel.create({
+        student_id: createClassMapDto.student_id,
+        classroom_id: createClassMapDto.classroom_id,
+      });
+      return result;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
   }
 
-  findAll() {
-    return `This action returns all classMaps`;
+  async findAll(): Promise<class_maps[]> {
+    return this.class_mapsModel.findAll({
+      where: {
+        isActive: true,
+      },
+    });
   }
 
   findOne(id: number) {
