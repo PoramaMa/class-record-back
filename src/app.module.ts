6b4153bbad61efb7a74e5319c82d1d2612fc8e94
Porama Arsama <porama.prm@gmail.com>
2024-05-13 00:55:31 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { class_maps } from './class_maps/schemas/class_maps.model';
 import { HomeController } from './home/home.controller';
 import { students } from './students/schemas/students.model';
 import { StudentsModule } from './students/students.module';
+require('dotenv').config();
 
 @Module({
   imports: [
@@ -20,11 +21,11 @@ import { StudentsModule } from './students/students.module';
         acquire: 30000,
         idle: 10000,
       },
-      host: '45.150.129.195',
+      host: process.env.DB_HOST,
       port: 3306,
-      username: 'root',
-      password: 'ZZ1RUfSQ6o33V-Yb',
-      database: 'class_record',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
       autoLoadModels: true,
       synchronize: true,
       models: [students, classrooms, class_maps],
